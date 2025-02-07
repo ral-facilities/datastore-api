43d91e6bf6056b45985866d97ac6aec34e5113d3
Mohsin <muhammad.mohsin@stfc.ac.uk>
2025-02-07 16:40:50 +0000

    added get_size to /transfer and updated tests

--- datastore_api/main.py
+++ datastore_api/main.py
@@ -272,12 +272,14 @@ def restore(
         "creating an FTS transfer"
     ),
     tags=["Transfer"],
+    response_model_exclude_none=True,
 )
 def transfer(
     source_key: SourceKey,
     destination_key: DestinationKey,
     transfer_request: TransferS3Request | TransferRequest,
     session_id: SessionIdDependency,
+    get_size: bool = False,
 ) -> TransferS3Response | TransferResponse:
     """Submit a request to transfer experimental data to the another location,
     creating an FTS transfer.
@@ -312,6 +314,8 @@ def transfer(
     message = "Submitted FTS transfer jobs for %s transfers with ids %s"
     LOGGER.info(message, restore_controller.total_transfers, restore_controller.job_ids)
 
+    if not get_size:
+        response.size = None
     return response
 
 
